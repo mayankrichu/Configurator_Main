0309bddf835fe8c10e745100435c420da3e6b3cc
mayankrichu <71438205+mayankrichu@users.noreply.github.com>
2023-03-07 13:35:36 +0100

    07/03/2023 Modified the html and the css

--- Configurator/routes.py
+++ Configurator/routes.py
@@ -1,20 +1,24 @@
 from flask import Blueprint, render_template, request, jsonify, redirect, url_for
 from Configurator.utils import get_skills
+import json
 
 configurator = Blueprint('configurator', __name__)
-
-
+independent_skill_file_path = '/Users/mayank/Downloads/task.json'
+dependent_skill_file_path = '/Users/mayank/Downloads/task.json'
+task_independent_skills = get_skills(independent_skill_file_path)
+task_dependent_skills = get_skills(dependent_skill_file_path)
 @configurator.route('/configurator')
 def Configurator():
-    independent_skill_file_path = '/Users/mayank/Downloads/task.json'
-    dependent_skill_file_path = '/Users/mayank/Downloads/task.json'
-    task_independent_skills = get_skills(independent_skill_file_path)
-    task_dependent_skills = get_skills(dependent_skill_file_path)
+
+
     return render_template('Configurator.html', task_independent_skills=task_independent_skills, task_dependent_skills = task_dependent_skills )
 
 @configurator.route('/configurator_submit' ,methods=['POST'])
 def Configurator_Submit():
 
     selected_configuration = request.form.get('values').split(',')
-    print(selected_configuration)
-    return render_template('Configurator.html')
+    print(selected_configuration[:-1])
+    selected_configuration_dict = {"Steps":selected_configuration}
+    with open('configuration_file.json', 'w') as f:
+        json.dump(selected_configuration_dict, f)
+    return render_template('Configurator.html', task_independent_skills=task_independent_skills, task_dependent_skills = task_dependent_skills)
